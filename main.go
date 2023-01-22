5d88e08c8479a6d0e809ed724f0b1239ed9835fe
Alex Della Ragione <adragione@gmail.com>
2023-01-22 16:45:02 -0500

    Dynamic input prompt and scheduling every 30 minutes

--- main.go
+++ main.go
@@ -5,23 +5,30 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
 	"os"
 	"time"
 
 	"github.com/go-co-op/gocron"
 	"github.com/joho/godotenv"
+	"github.com/tjarratt/babble"
 )
 
 func LoadEnvVariables() {
-
 	err := godotenv.Load()
-
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
 }
+func generatePrompt() string {
+	babbler := babble.NewBabbler()
+	babbler.Separator = " "
+	babbler.Count = rand.Intn(10)
+	prompt := babbler.Babble()
 
+	return (prompt)
+}
 func apiWarmup() {
 	type Input struct {
 		Prompt                 string  `json:"prompt"`
@@ -36,12 +43,11 @@ func apiWarmup() {
 		Version string `json:"version"`
 		Input   Input  `json:"input"`
 	}
-
 	posturl := "https://api.replicate.com/v1/predictions"
 	values := Post{
 		Version: "c24bbf13332c755f9e1c8b3f10c7f438889145def57d554a74ea751dc5e3b509",
 		Input: Input{
-			Prompt:                 "I like turtles",
+			Prompt:                 generatePrompt(),
 			Width:                  128,
 			Height:                 128,
 			Prompt_Strength:        .5,
@@ -51,18 +57,15 @@ func apiWarmup() {
 		},
 	}
 	json_data, err := json.Marshal(values)
-
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	r, err := http.NewRequest("POST", posturl, bytes.NewBuffer(json_data))
 	if err != nil {
 		panic(err)
 	}
 	r.Header.Add("Authorization", "Token "+os.Getenv("NUXT_REP_API_KEY"))
 	r.Header.Add("Content-Type", "application/json")
-
 	client := &http.Client{}
 	resp, err := client.Do(r)
 	if err != nil {
@@ -70,26 +73,21 @@ func apiWarmup() {
 	}
 	defer resp.Body.Close()
 	var res map[string]interface{}
-
 	json.NewDecoder(resp.Body).Decode(&res)
-
-	fmt.Println(res)
+	fmt.Println(res["urls"].(map[string]interface{})["get"])
 }
-
 func runCronJobs() {
 	s := gocron.NewScheduler(time.UTC)
 
-	s.Every(10).Minutes().Do(func() {
+	s.Every(30).Minutes().Do(func() {
 		apiWarmup()
 	})
 
 	s.StartBlocking()
 }
-
 func init() {
 	LoadEnvVariables()
 }
-
 func main() {
 	runCronJobs()
 }
